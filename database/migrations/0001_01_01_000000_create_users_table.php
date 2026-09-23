e8114783f108f1f7d133318ffa714bc47bc67ca3
valler4 <valler1270@gmail.com>
2026-09-23 08:32:36 +0300

    edited gender to nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->string('gender');
+            $table->string('gender')->nullable();
             $table->enum('role', ['user', 'admin']);
             $table->rememberToken();
             $table->timestamps();
