79d3fec9ed0b43a47078d2694567331dc7458931
valler4 <valler1270@gmail.com>
2026-09-24 12:17:59 +0300

    add IsAdmin middleware for admin role verification

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() && $request->user()->role === 'admin') {
+            return $next($request);
+        };
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Unauthorized',
+        ], 403);
+    }
+}
